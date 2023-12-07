668335edf82a70899eb381af7f374310593162a3
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2023-12-07 21:03:19 +0300

    Make Laptop migration

--- challenges/models.py
+++ challenges/models.py
@@ -8,3 +8,18 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Laptop(models.Model):
+    brand = models.CharField(
+        max_length=20,
+        choices=[('lenovo', 'Lenovo'), ('dell', 'Dell'), ('acer', 'Acer')]
+    )
+    production_date = models.DateField()
+    ram_mb = models.PositiveIntegerField()
+    hdd_mb = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField()
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
